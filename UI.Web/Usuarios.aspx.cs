1589f2ade124386fcda861f89ba92755f7acb0ed
aragonzalez <ara.gonzalez92@yahoo.com.ar>
2015-10-14 11:51:57 -0300

    Lab-06- paso 28
    Agregar propiedades a la presentación y modificar un usuario
    seleccionado desde la grilla.

--- UI.Web/Usuarios.aspx.cs
+++ UI.Web/Usuarios.aspx.cs
@@ -39,5 +39,75 @@ namespace UI.Web
                 this.LoadGrid();
             }
         }
+
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+
+        private Usuario Entity
+        {
+            get;
+            set;
+        }
+        
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool isEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.LogicUsuario.GetOne(id);
+            this.nombreTextBox.Text = this.Entity.Nombre;
+            this.apellidoTextBox.Text = this.Entity.Apellido;
+            this.emailTextBox.Text = this.Entity.Email;
+            this.habilitadoCheckBox.Checked = this.Entity.Habilitado;
+            this.nombreUsuarioTextBox.Text = this.Entity.NombreUsuario;
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.isEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+            }
+        }
     }
 }
