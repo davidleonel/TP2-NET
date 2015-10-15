e34528180131d5146d7b5735a3d3967eeec1276a
aragonzalez <ara.gonzalez92@yahoo.com.ar>
2015-10-15 11:02:30 -0300

    Solo comentarios
    commit alpedo

--- UI.Escritorio/UsuariosEscritorio.cs
+++ UI.Escritorio/UsuariosEscritorio.cs
@@ -48,7 +48,7 @@ namespace UI.Escritorio
         }
 
 
-        // FALTA PASO 24
+        // FALTA PASO 24 //VER SI ESTOS ESTAN DE MAS
         //private void toolStripButton1_Click(object sender, EventArgs e)
         //{
         //    UsuarioEscritorio usrEsc = new UsuarioEscritorio(ApplicationForm.ModoForm.Alta);
