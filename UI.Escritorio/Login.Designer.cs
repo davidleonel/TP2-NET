58bc859bd476890a6deddd2dd89367b3521c0a89
aragonzalez <ara.gonzalez92@yahoo.com.ar>
2015-11-11 11:07:33 -0300

    Login

---
+++ UI.Escritorio/Login.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace UI.Escritorio
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpLogin = new System.Windows.Forms.TableLayoutPanel();
+            this.lblBienv = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tlpLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpLogin
+            // 
+            this.tlpLogin.ColumnCount = 2;
+            this.tlpLogin.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpLogin.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpLogin.Controls.Add(this.lblBienv, 0, 0);
+            this.tlpLogin.Controls.Add(this.lblUsuario, 0, 1);
+            this.tlpLogin.Controls.Add(this.lblPass, 0, 2);
+            this.tlpLogin.Controls.Add(this.btnIngresar, 1, 3);
+            this.tlpLogin.Controls.Add(this.txtUsuario, 1, 1);
+            this.tlpLogin.Controls.Add(this.textBox2, 1, 2);
+            this.tlpLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpLogin.Location = new System.Drawing.Point(0, 0);
+            this.tlpLogin.Name = "tlpLogin";
+            this.tlpLogin.RowCount = 4;
+            this.tlpLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.55556F));
+            this.tlpLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+            this.tlpLogin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+            this.tlpLogin.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpLogin.Size = new System.Drawing.Size(284, 261);
+            this.tlpLogin.TabIndex = 0;
+            // 
+            // lblBienv
+            // 
+            this.lblBienv.AutoSize = true;
+            this.tlpLogin.SetColumnSpan(this.lblBienv, 2);
+            this.lblBienv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblBienv.Location = new System.Drawing.Point(3, 0);
+            this.lblBienv.Name = "lblBienv";
+            this.lblBienv.Size = new System.Drawing.Size(278, 128);
+            this.lblBienv.TabIndex = 0;
+            this.lblBienv.Text = "¡Bienvenidos al Sistema de Gestión Academica!";
+            this.lblBienv.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(96, 128);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            this.lblUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPass
+            // 
+            this.lblPass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(78, 179);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(61, 13);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "Contraseña";
+            this.lblPass.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIngresar.Location = new System.Drawing.Point(206, 233);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 3;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(145, 131);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(145, 182);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tlpLogin);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.tlpLogin.ResumeLayout(false);
+            this.tlpLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpLogin;
+        private System.Windows.Forms.Label lblBienv;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
